a0f3fd50b103c5762aee84d42932218271e9ca41
Saad Asif <saadasif2121@gmail.com>
2020-06-08 18:05:46 -0700

    adding favico

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import HttpsRedirect from 'react-https-redirect';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <HttpsRedirect>
+    <App />
+  </HttpsRedirect>,
+  document.getElementById('root')
+);
